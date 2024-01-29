63df8efb4b7e0259e86aa26646b685b41a871f79
SoraNoKage <llsoranokagell@gmail.com>
2024-01-29 10:31:36 +0300

    added password validator

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,5 +1,6 @@
 import bcrypt
-from pydantic import BaseModel, EmailStr
+import re
+from pydantic import BaseModel, validator, EmailStr
 # write normal password schema
 
 
@@ -7,6 +8,20 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+    @validator('password')
+    def check_password(cls, v):
+        if len(v) < 8:
+            raise ValueError("Password length must be at least 8 characters")
+        if not any(char.isupper() for char in v):
+            raise ValueError("Password must contain at least 1 uppercase letter")
+        if not any(char.islower() for char in v):
+            raise ValueError("Password must contain at least 1 lowercase letter")
+        if not any(char.isdigit() for char in v):
+            raise ValueError("Password must contain at least 1 digit")
+        if not re.search(r"[!@#$%^&*()-_+=<>,.?/:;]", v):
+            raise ValueError("Password must contain at least 1 punctuation mark")
+        return v
 
 
 class UserLogin(BaseModel):
